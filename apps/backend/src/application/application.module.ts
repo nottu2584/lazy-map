841bb3f02e81fdcb62f2d68a6832844ae4878a4e
Xavi Giménez <nottu.2584@gmail.com>
2025-08-19 12:34:19 +0200

    feat: create ApplicationModule with use cases and application services

---
+++ apps/backend/src/application/application.module.ts
@@ -0,0 +1,98 @@
+import { Module } from '@nestjs/common';
+import { InfrastructureModule } from '../infrastructure/infrastructure.module';
+import { 
+  GenerateMapUseCase, 
+  ValidateMapSettingsUseCase,
+  GetMapUseCase,
+  GetMapTileUseCase,
+  ListMapsUseCase,
+  MapApplicationService
+} from '@lazy-map/application';
+
+@Module({
+  imports: [InfrastructureModule],
+  providers: [
+    // Use cases
+    {
+      provide: GenerateMapUseCase,
+      useFactory: (
+        mapGenService,
+        vegetationGenService, 
+        mapPersistence, 
+        randomGen, 
+        notificationPort
+      ) => {
+        return new GenerateMapUseCase(
+          mapGenService, 
+          vegetationGenService, 
+          mapPersistence, 
+          randomGen, 
+          notificationPort
+        );
+      },
+      inject: [
+        'IMapGenerationService',
+        'IVegetationGenerationService',
+        'IMapPersistencePort',
+        'IRandomGeneratorService',
+        'INotificationPort',
+      ],
+    },
+    {
+      provide: ValidateMapSettingsUseCase,
+      useFactory: () => {
+        return new ValidateMapSettingsUseCase();
+      },
+    },
+    {
+      provide: GetMapUseCase,
+      useFactory: (mapPersistence) => {
+        return new GetMapUseCase(mapPersistence);
+      },
+      inject: ['IMapPersistencePort'],
+    },
+    {
+      provide: GetMapTileUseCase,
+      useFactory: (mapPersistence) => {
+        return new GetMapTileUseCase(mapPersistence);
+      },
+      inject: ['IMapPersistencePort'],
+    },
+    {
+      provide: ListMapsUseCase,
+      useFactory: (mapPersistence) => {
+        return new ListMapsUseCase(mapPersistence);
+      },
+      inject: ['IMapPersistencePort'],
+    },
+    
+    // Application services
+    {
+      provide: MapApplicationService,
+      useFactory: (
+        generateMapUseCase,
+        validateMapSettingsUseCase,
+        getMapUseCase,
+        getMapTileUseCase,
+        listMapsUseCase
+      ) => {
+        return new MapApplicationService(
+          generateMapUseCase,
+          validateMapSettingsUseCase,
+          getMapUseCase,
+          getMapTileUseCase,
+          listMapsUseCase
+        );
+      },
+      inject: [
+        GenerateMapUseCase,
+        ValidateMapSettingsUseCase,
+        GetMapUseCase,
+        GetMapTileUseCase,
+        ListMapsUseCase
+      ],
+    },
+  ],
+  exports: [MapApplicationService],
+})
+export class ApplicationModule {}

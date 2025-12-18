4232f79141d116a0e210051b0d4200ad8feeefd5
Xavi Giménez <nottu.2584@gmail.com>
2025-12-18 10:11:25 +0100

    feat(backend): fix JWT handling

--- apps/backend/src/modules/auth/guards/jwt-auth.guard.ts
+++ apps/backend/src/modules/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext) {
+    const result = super.canActivate(context);
+    return result;
+  }
+
+  handleRequest(err: any, user: any, info: any) {
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+    return user;
+  }
+}

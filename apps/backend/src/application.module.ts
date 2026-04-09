5cc08a00ed8c9bdc6f801ddd6609dd46753b0c30
Xavi Giménez <nottu.2584@gmail.com>
2026-04-09 16:12:35 +0200

    refactor(application): remove obsolete use cases for map tile and validation settings

--- apps/backend/src/application.module.ts
+++ apps/backend/src/application.module.ts
@@ -5,7 +5,6 @@ import {
   DeleteUserUseCase,
   DiscordSignInUseCase,
   GenerateTacticalMapUseCase,
-  GetMapTileUseCase,
   GetMapUseCase,
   GetUserMapsUseCase,
   GetUserPermissionsUseCase,
@@ -26,16 +25,14 @@ import {
   SaveMapUseCase,
   SuspendUserUseCase,
   UpdateUserUseCase,
-  ValidateMapSettingsUseCase,
   ValidateSeedUseCase,
 } from '@lazy-map/application';
-import { Module } from '@nestjs/common';
-import { InfrastructureModule } from './infrastructure.module';
-import { PostgresMapRepository } from '@lazy-map/infrastructure';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { MapEntity } from '@lazy-map/infrastructure';
-import { Repository } from 'typeorm';
 import { ILogger, ITacticalMapConverter, TacticalMapConverter } from '@lazy-map/domain';
+import { MapEntity, PostgresMapRepository } from '@lazy-map/infrastructure';
+import { Module } from '@nestjs/common';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { InfrastructureModule } from './infrastructure.module';
 
 // Helper function to check if database should be used
 const shouldUseDatabase = () => {
@@ -54,7 +51,7 @@ const shouldUseDatabase = () => {
         hydrologyService,
         vegetationService,
         structuresService,
-        logger
+        logger,
       ) => {
         return new GenerateTacticalMapUseCase(
           geologyService,
@@ -62,7 +59,7 @@ const shouldUseDatabase = () => {
           hydrologyService,
           vegetationService,
           structuresService,
-          logger
+          logger,
         );
       },
       inject: [
@@ -71,7 +68,7 @@ const shouldUseDatabase = () => {
         'IHydrologyLayerService',
         'IVegetationLayerService',
         'IStructuresLayerService',
-        'ILogger'
+        'ILogger',
       ],
     },
 
@@ -92,16 +89,21 @@ const shouldUseDatabase = () => {
               mapEntityRepository: Repository<MapEntity>,
               generateMapUseCase: GenerateTacticalMapUseCase,
               tileConverter: ITacticalMapConverter,
-              logger: ILogger
+              logger: ILogger,
             ) => {
               return new PostgresMapRepository(
                 mapEntityRepository,
                 generateMapUseCase,
                 tileConverter,
-                logger
+                logger,
               );
             },
-            inject: [getRepositoryToken(MapEntity), GenerateTacticalMapUseCase, 'ITacticalMapConverter', 'ILogger'],
+            inject: [
+              getRepositoryToken(MapEntity),
+              GenerateTacticalMapUseCase,
+              'ITacticalMapConverter',
+              'ILogger',
+            ],
           },
           // Also provide IMapPersistencePort as a wrapper around PostgresMapRepository
           // This allows use cases that depend on IMapPersistencePort to work with PostgreSQL
@@ -117,8 +119,12 @@ const shouldUseDatabase = () => {
                 mapExists: (mapId: any) => mapRepository.exists(mapId),
                 findByOwner: (userId: any, _limit?: number) => mapRepository.findByOwnerId(userId),
                 getMapCount: () => mapRepository.count(),
-                listMaps: async () => { throw new Error('Not implemented'); },
-                beginTransaction: async () => { throw new Error('Not implemented'); },
+                listMaps: async () => {
+                  throw new Error('Not implemented');
+                },
+                beginTransaction: async () => {
+                  throw new Error('Not implemented');
+                },
               };
             },
             inject: ['IMapRepository'],
@@ -126,12 +132,6 @@ const shouldUseDatabase = () => {
         ]
       : []),
 
-    {
-      provide: ValidateMapSettingsUseCase,
-      useFactory: () => {
-        return new ValidateMapSettingsUseCase();
-      },
-    },
     {
       provide: ValidateSeedUseCase,
       useFactory: () => {
@@ -148,13 +148,6 @@ const shouldUseDatabase = () => {
       provide: GetMapUseCase,
       useFactory: (mapPersistence) => {
         return new GetMapUseCase(mapPersistence);
-      },
-      inject: ['IMapPersistencePort'],
-    },
-    {
-      provide: GetMapTileUseCase,
-      useFactory: (mapPersistence) => {
-        return new GetMapTileUseCase(mapPersistence);
       },
       inject: ['IMapPersistencePort'],
     },
@@ -205,14 +198,24 @@ const shouldUseDatabase = () => {
     {
       provide: GoogleSignInUseCase,
       useFactory: (userRepository, googleOAuthService, authenticationService, logger) => {
-        return new GoogleSignInUseCase(userRepository, googleOAuthService, authenticationService, logger);
+        return new GoogleSignInUseCase(
+          userRepository,
+          googleOAuthService,
+          authenticationService,
+          logger,
+        );
       },
       inject: ['IUserRepository', 'IGoogleOAuthPort', 'IAuthenticationPort', 'ILogger'],
     },
     {
       provide: DiscordSignInUseCase,
       useFactory: (userRepository, discordOAuthService, authenticationService, logger) => {
-        return new DiscordSignInUseCase(userRepository, discordOAuthService, authenticationService, logger);
+        return new DiscordSignInUseCase(
+          userRepository,
+          discordOAuthService,
+          authenticationService,
+          logger,
+        );
       },
       inject: ['IUserRepository', 'IDiscordOAuthPort', 'IAuthenticationPort', 'ILogger'],
     },
@@ -239,10 +242,31 @@ const shouldUseDatabase = () => {
     },
     {
       provide: CompleteGoogleSignInUseCase,
-      useFactory: (userRepository, oauthTokenRepository, googleOAuthService, authenticationService, tokenEncryptionService, logger) => {
-        return new CompleteGoogleSignInUseCase(userRepository, oauthTokenRepository, googleOAuthService, authenticationService, tokenEncryptionService, logger);
+      useFactory: (
+        userRepository,
+        oauthTokenRepository,
+        googleOAuthService,
+        authenticationService,
+        tokenEncryptionService,
+        logger,
+      ) => {
+        return new CompleteGoogleSignInUseCase(
+          userRepository,
+          oauthTokenRepository,
+          googleOAuthService,
+          authenticationService,
+          tokenEncryptionService,
+          logger,
+        );
       },
-      inject: ['IUserRepository', 'IOAuthTokenRepository', 'IGoogleOAuthPort', 'IAuthenticationPort', 'ITokenEncryptionPort', 'ILogger'],
+      inject: [
+        'IUserRepository',
+        'IOAuthTokenRepository',
+        'IGoogleOAuthPort',
+        'IAuthenticationPort',
+        'ITokenEncryptionPort',
+        'ILogger',
+      ],
     },
     {
       provide: InitiateDiscordSignInUseCase,
@@ -253,10 +277,31 @@ const shouldUseDatabase = () => {
     },
     {
       provide: CompleteDiscordSignInUseCase,
-      useFactory: (userRepository, oauthTokenRepository, discordOAuthService, authenticationService, tokenEncryptionService, logger) => {
-        return new CompleteDiscordSignInUseCase(userRepository, oauthTokenRepository, discordOAuthService, authenticationService, tokenEncryptionService, logger);
+      useFactory: (
+        userRepository,
+        oauthTokenRepository,
+        discordOAuthService,
+        authenticationService,
+        tokenEncryptionService,
+        logger,
+      ) => {
+        return new CompleteDiscordSignInUseCase(
+          userRepository,
+          oauthTokenRepository,
+          discordOAuthService,
+          authenticationService,
+          tokenEncryptionService,
+          logger,
+        );
       },
-      inject: ['IUserRepository', 'IOAuthTokenRepository', 'IDiscordOAuthPort', 'IAuthenticationPort', 'ITokenEncryptionPort', 'ILogger'],
+      inject: [
+        'IUserRepository',
+        'IOAuthTokenRepository',
+        'IDiscordOAuthPort',
+        'IAuthenticationPort',
+        'ITokenEncryptionPort',
+        'ILogger',
+      ],
     },
 
     // Admin use cases
@@ -323,7 +368,6 @@ const shouldUseDatabase = () => {
       },
       inject: ['IUserRepository'],
     },
-
   ],
   exports: [
     // Export Map Use Cases
